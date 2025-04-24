bbe89e3852c4b08bd0349a9f105ed1029e3a6731
Nelicah <candezmr2@gmail.com>
2025-04-24 11:34:56 +0200

    ejercicios de hoy

--- js/main.js
+++ js/main.js
@@ -53,3 +53,30 @@ kittenPlus.addEventListener("click", () => {
   formKitten.classList.toggle('collapsed');
 });
 
+const cancelButton = document.querySelector('.button-cancel');
+
+const formKittenAdd = document.querySelector('.form-kitten-add');
+
+cancelButton.addEventListener("click", () => {
+  formKitten.classList.add('collapsed');
+  formKittenAdd.reset(); //resetea el formulario
+});
+
+const inputSearchDesc = document.querySelector('js_in_search_desc');
+
+const buttonSearch = document.querySelector('js_button-search');
+
+buttonSearch.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const descrSearchText = inputSearchDesc.value;
+});
+
+const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente."
+
+const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo."
+
+const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta."
+
+if (kittenDesc1.includes(descrSearchText)) {
+  kittenBlue.innerHTML = kittenBlue.innerHTML;
+};
